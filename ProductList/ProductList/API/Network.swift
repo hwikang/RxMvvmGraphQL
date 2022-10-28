8db5773bb75cc82525bfbe19e93704e1a74553d6
Simon <khdrogba@gmail.com>
2022-10-28 17:14:11 +0900

    Feature: Apollo 클라이언트 생성 & ProductList, SupplierList 쿼리 작성 	- 인터셉터에 UUID 헤더 추가

---
+++ ProductList/ProductList/API/Network.swift
@@ -0,0 +1,68 @@
+//
+//  Network.swift
+//  ProductList
+//
+//  Created by Simon on 2022/10/28.
+//
+
+import Foundation
+import Apollo
+
+class Network {
+    static let shared = Network()
+    
+    private(set) lazy var apollo: ApolloClient = {
+        guard let url = URL(string: "https://test.recruit.croquis.com:28501"),
+        let uuid = UIDevice.current.identifierForVendor?.uuidString else { fatalError("Create Apollo Client Error")}
+        
+        let configuration = URLSessionConfiguration.default
+
+        let uuidHeader = ["Croquis-UUID": uuid]
+        configuration.httpAdditionalHeaders = uuidHeader
+        print("uuidHeader \(uuidHeader)")
+        
+        let store = ApolloStore()
+
+        let interceptorProvider = NetworkInterceptorsProvider(
+                   interceptors: [UUIDInterceptor(uuid: uuid)],
+                   store: store
+               )
+       let networkTransport = RequestChainNetworkTransport(interceptorProvider: interceptorProvider, endpointURL: url)
+        return ApolloClient(networkTransport: networkTransport, store: store)
+
+    }()
+}
+
+
+class UUIDInterceptor: ApolloInterceptor {
+  
+    let uuid: String
+    init(uuid: String) {
+        self.uuid = uuid
+    }
+    
+    func interceptAsync<Operation>(chain: RequestChain, request: HTTPRequest<Operation>, response: HTTPResponse<Operation>?, completion: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) where Operation : GraphQLOperation {
+        
+        request.addHeader(name: "Croquis-UUID", value: self.uuid)
+        chain.proceedAsync(request: request, response: response, completion: completion)
+    }
+    
+}
+
+
+class NetworkInterceptorsProvider: DefaultInterceptorProvider {
+    let interceptors: [ApolloInterceptor]
+    init(interceptors: [ApolloInterceptor], store: ApolloStore) {
+        self.interceptors = interceptors
+        super.init(store: store)
+    }
+    
+    override func interceptors<Operation>(for operation: Operation) -> [ApolloInterceptor] where Operation : GraphQLOperation {
+        var superInterceptors = super.interceptors(for: operation)
+        self.interceptors.forEach { interceptor in
+            superInterceptors.insert(interceptor, at: 0)
+        }
+        return superInterceptors
+    }
+    
+}
